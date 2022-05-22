679a9a2b02f604db2cdaa48ea6ee00148e622d79
SimarjeeSinghJohar <85599473+SimarjeeSinghJohar@users.noreply.github.com>
2022-05-22 00:13:44 -0400

    Updated styling

--- Portfolio/.next/static/chunks/webpack.js
+++ Portfolio/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eeaadb97a561a5c2695d"; }
+/******/ 		__webpack_require__.h = function() { return "58c5f66851b1cce01967"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
